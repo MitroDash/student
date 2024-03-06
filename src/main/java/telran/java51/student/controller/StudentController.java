dc257b2102ff5de1e34ffb5ae930f7eaa2afc123
mitrodash <mitrodash@10.100.102.45>
2024-03-06 15:07:34 +0200

    final version

--- src/main/java/telran/java51/student/controller/StudentController.java
+++ src/main/java/telran/java51/student/controller/StudentController.java
@@ -3,7 +3,6 @@ package telran.java51.student.controller;
 import java.util.List;
 import java.util.Set;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
